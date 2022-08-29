79021c205a6ae9540f3718267f6a5a28cbc57d40
Rishabh Mathe <rishabh.mathe@nagarro.com>
2022-08-29 14:54:11 +0530

    modification in build script

---
+++ src/main/java/com/rishabh/commons/Main.java
@@ -0,0 +1,7 @@
+package com.rishabh.commons;
+
+class Main{
+	public static void main(String args[]) {
+		System.out.println("hello");
+	}
+}
---
+++ src/main/java/com/rishabh/commons/OsCommonClass.java
@@ -0,0 +1,7 @@
+package com.rishabh.commons;
+
+public class OsCommonClass {
+	public void m1() {
+		System.out.println("I'm Method 1");
+	}
+}
